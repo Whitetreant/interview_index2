7b8e240736152f36495981005c4b398bebca4798
White <k.viwatsquadx@gmail.com>
2026-01-17 18:17:36 +0700

    - add loading

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center p-10">
+      <p className="text-lg animate-pulse">Loading Pokémon...</p>
+    </div>
+  );
+}
